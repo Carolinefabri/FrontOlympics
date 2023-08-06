56647186e6c68a259a50ea0da00279877b4d03e2
Brian Adams <brianadamsmaino68@gmail.com>
2023-08-06 19:27:49 +0200

    Sidebar

--- src/pages/AllSportsPage.jsx
+++ src/pages/AllSportsPage.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom"; 
-
+import { Link } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const AllSportsPage = () => {
   const [sports, setSports] = useState([]);
 
   async function fetchSports() {
     try {
-      const response = await axios.get('http://localhost:5005/sports');
+      const response = await axios.get("http://localhost:5005/sports");
       if (response.status === 200) {
         setSports(response.data.sports);
       }
@@ -22,21 +22,29 @@ const AllSportsPage = () => {
   }, []);
 
   return (
-    <div className="all-sports">
-      <h1>All Sports</h1>
-      {sports.map(sport => (
-        <div key={sport.id} className="sport-card"
-          style={{ display: 'block', border: '1px solid lightgrey', margin: '1rem 0' }}
+    <div>
+      <Sidebar />
+      <div className="all-sports">
+        <h1>All Sports</h1>
+        {sports.map((sport) => (
+          <div
+            key={sport.id}
+            className="sport-card"
+            style={{
+              display: "block",
+              border: "1px solid lightgrey",
+              margin: "1rem 0",
+            }}
           >
-            
-          <Link to={`/allsports/${sport.id}`}>
-            <img src={sport.image} alt={sport.name} className="sport-image" />
-            <span className="sport-name">{sport.name}</span>
-          </Link>
-        </div>
-      ))}
+            <Link to={`/allsports/${sport.id}`}>
+              <img src={sport.image} alt={sport.name} className="sport-image" />
+              <span className="sport-name">{sport.name}</span>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
-  ) 
-}
+  );
+};
 
 export default AllSportsPage;
