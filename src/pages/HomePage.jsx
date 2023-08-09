78d702c59352c2b939c05cf565d40ca4f89196e4
“Caroline <carolinefabri.dev@hotmail.com>
2023-08-09 08:44:34 +0200

    dois pontos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,22 +14,22 @@ const HomePage = () => {
     <div className="home-container">
       <NavBar />
       <div className="image-container">
-        <img className="homephoto" src="../public/images/home.png" alt="homepage" />
+        <img className="homephoto" src="../images/home.png" alt="homepage" />
         <button className="register" onClick={handleGetStarted} >
           <strong>JOIN NOW</strong>
         </button>
       </div>
       <div>
-      <img className="serviceshome" src="./public/images/services.png" alt="homepage" />
+      <img className="serviceshome" src="../images/services.png" alt="homepage" />
       </div>
       <div>
-      <img className="sportshome" src="./public/images/sports.png" alt="homepage" />
+      <img className="sportshome" src="../images/sports.png" alt="homepage" />
       </div>
       <div>
-      <img className="testemunhahome" src="./public/images/testemunha.png" alt="homepage" />
+      <img className="testemunhahome" src="../images/testemunha.png" alt="homepage" />
       </div>
       <div>
-      <img className="subscribehome" src="./public/images/subscribe.png" alt="homepage" />
+      <img className="subscribehome" src="../images/subscribe.png" alt="homepage" />
       </div>
  
 
