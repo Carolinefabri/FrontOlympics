3cf9054c3e1dd65b74a89f5119fae854385d4162
“Caroline <carolinefabri.dev@hotmail.com>
2023-08-11 12:56:04 +0200

    admin

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,9 +1,15 @@
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import CountdownTimer from '../components/CountdownTimer';
-import pageAdminImage from "/images/pageadmi.jpg";
 import '../App.css'; // Importe o arquivo de estilos global
+import pageAdminImage from "/images/hero.png"; 
+import newsImage from "/images/wordsnews.png"; 
+import newsImage1 from "/images/wordsnews1.png"; 
+import newsImage2 from "/images/wordsnews2.png"; 
+import bookImage from "/images/book.png"; 
+import parisImage from "/images/paris.png"; 
 
 const AdminPage = ({ user }) => {
   const navigate = useNavigate();
@@ -17,6 +23,7 @@ const AdminPage = ({ user }) => {
     <div className="App"> {/* Use a classe "App" para aplicar estilos globais */}
     
     <Sidebar />
+  
       <div className="countdown-container">
         <h2 className="countdown-title"> Paris 2024 </h2>
         <h3>The Journey Begins in...</h3>
@@ -25,8 +32,22 @@ const AdminPage = ({ user }) => {
       <section>
     <img className="pageAdminImage " src={pageAdminImage} alt="pageAdminImage" /> 
     </section>
-    
-      <button className="bttadmin" onClick={handleGetStarted}>Get Started</button>
+    <section>
+    <img className="newsImage " src={newsImage} alt="newsImageImage" /> 
+    </section>
+    <section>
+    <img className="newsImage1 " src={newsImage1} alt="newsImageImage1" /> 
+    </section>
+    <section>
+    <img className="newsImage2 " src={newsImage2} alt="newsImageImage" /> 
+    </section>
+    <section>
+    <img className="bookImage " src={bookImage} alt="newsImageImage" /> 
+    </section>
+    <section>
+    <img className="parisImage " src={parisImage} alt="parisImage" /> 
+    </section>
+      <button className="bttadmin" onClick={handleGetStarted}><strong>Get Started</strong></button>
     </div>
   );
 };
